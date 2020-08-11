fe6e7cca1e40f0b288b0c83e009e7cff4af86c47
Peter Nguyen <spacechannel6@gmail.com>
2020-08-11 17:47:08 +0100

    created lose screen ui widget, add it to screen when losing the game in player controller.

--- Source/SimpleShooter/ShooterPlayerController.cpp
+++ Source/SimpleShooter/ShooterPlayerController.cpp
@@ -3,12 +3,19 @@
 
 #include "ShooterPlayerController.h"
 #include "TimerManager.h"
+#include "Blueprint/UserWidget.h"
 
 void AShooterPlayerController::GameHasEnded(class AActor* EndGameFocus, bool bIsWinner)
 {
 	Super::GameHasEnded(EndGameFocus,bIsWinner);
 
 	UE_LOG(LogTemp, Warning, TEXT("Game Has Ended"));
+
+	UUserWidget * LoseScreen = CreateWidget(this, LoseScreenClass);
+	if (LoseScreen != nullptr)
+	{
+		LoseScreen->AddToViewport();
+	}
 
 	GetWorldTimerManager().SetTimer(RestartTimer, this, &APlayerController::RestartLevel, RestartDelay);
 }
